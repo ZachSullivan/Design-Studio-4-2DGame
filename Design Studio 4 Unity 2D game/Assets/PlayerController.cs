fa6542a7af7f78f3c6d788b22d825cb6cb153040
ZachSullivan <zach.r.sullivan@gmail.com>
2015-10-20 14:25:17 -0400

    First commit

---
+++ Design Studio 4 Unity 2D game/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//defines the speed at which the player will move
+	public int speed;
+
+	public float floatHeight;
+	public float liftForce;
+	public float damping;
+	public Rigidbody2D rb2D;
+
+	void Start () {
+		speed = 10;
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		Move ();
+
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up);
+		if (hit.collider != null ) {
+			Debug.Log (hit.transform.gameObject.name);
+		}
+	}
+
+	void Move (){
+
+
+		//Update main character movement through player key commands
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate ((Time.deltaTime * speed), 0.0f, 0.0f);
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate ((Time.deltaTime * -speed), 0.0f, 0.0f);
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			transform.Translate (0.0f, (Time.deltaTime * speed), 0.0f);
+		}
+
+
+	}
+
+}
